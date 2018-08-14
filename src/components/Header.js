315e1d01e8772e6c2a38bd54b1dab7dc49137992
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-08-14 23:58:47 +0200

    ready to deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
   return (
     <div className="header">
       
-      <h1 className="header__title">What Would <img src="./img/chuck-norris.png"/> Do?</h1> 
+      <h1 className="header__title">What Would <img src="./chuck-norris.png"/> Do?</h1> 
       {props.subtitle && <blockquote className="blockquote"><p> <em>{props.subtitle}</em></p><footer className="blockquote-footer">—Totally accurate fact about <cite title="Source Title">Chuck Norris</cite></footer>
 </blockquote>}
     
