1e623c73649a135bab363662bc0e1b134a70798e
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-08-14 23:48:33 +0200

    final

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require("express")
 const path = require("path")
 const app = new express()
 const publicPath = path.join(__dirname, "..", "public")
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath))
 app.get("*",(req, res) => {
     res.sendFile(path.join(publicPath, "index.html"))
 })
-app.listen(3000,() => {console.log("server is up")})
+app.listen(port,() => {console.log("server is up")})
